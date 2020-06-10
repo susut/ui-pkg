55b121301bbb02ff8a3dfe4123d2f1e28df9ec9a
chensuting <chensuting@ruqimobility.com>
2020-06-10 17:59:15 +0800

    chore: webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 let dev = merge(common, {
-    devServer: {
-        contentBase: path.join(__dirname, "dist")
-    }
+    mode: 'production',
+    entry: {
+        'index': path.join(__dirname, 'src', 'main.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash].js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve('./src/index.html')
+        })
+    ],
 });
 module.exports = dev;
